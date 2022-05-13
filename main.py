104a2c6846ed141bac40ceda92c1fc3e82d4a484
fu-kunn <sky53908@gmail.com>
2022-05-13 00:55:13 +0000

    [8 文字列の表現]

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-initial_balance = 10000
-RATE = 1.1
-
-print(initial_balance)
-print(initial_balance * RATE)
-print(initial_balance * RATE * RATE)
-
-# initial_balance = 11000
-initial_balance += 11000
-
-print(initial_balance)
-print(initial_balance * RATE)
-print(initial_balance * RATE * RATE)
+print("""
+It's
+a
+pen.""")
