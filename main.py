368726c1b3148a7569e9277761459af6fc6e3b21
fu-kunn <sky53908@gmail.com>
2022-05-14 03:07:48 +0000

    [19-matchとifの組み合わせ]

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ initial_balance = int(input("Initial Balance? "))
 #     RATE = 1.1
 # else:
 #     RATE = 1.01
-RATE = 1.1 if initial_balance >= 100_000 else 1.01
+# RATE = 1.1 if initial_balance >= 100_000 else 1.01 
 # 入力した値によって1.1または1.01の利率を表示している
+
+match initial_balance:
+    case n if n >= 100_000:
+        RATE = 1.1
+    case n if n >= 80_000:
+        RATE = 1.08
+    case n if n >= 60_000:
+        RATE = 1.06    
+    case _:
+        RATE = 1.01
+
 print(f"Curent rate: {RATE:.2f}")
 print(f"Year 0: {initial_balance:,.2f}")
 print(f"Year 1: {initial_balance * RATE:,.2f}")
