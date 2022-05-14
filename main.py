5297963b6fd85aa351eb1a1c1d1696def670c522
fu-kunn <sky53908@gmail.com>
2022-05-14 02:18:31 +0000

    [12 f文字列のオプション]

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-num = input("Number? ")
-# print(type(num))
-print(float(num) * 2)
+initial_barance = int(input("Initial Balance? "))
+RATE = 1.1
+print(f"Year 0: {initial_barance:,.2f}")
+print(f"Year 1: {initial_barance * RATE:,.2f}")
+print(f"Year 2: {initial_barance * RATE * RATE:,.2f}")
