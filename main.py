8a8ad8be4462f09df53d2be4f2dfb1425e14f3f0
fu-kunn <sky53908@gmail.com>
2022-05-14 02:52:30 +0000

    [17-パターンマッチ]

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-signal = input("Sgnal color? ")
+# signal = input("Sgnal color? ")
 
-if signal == "red":
-    print("Stop")
-elif signal == "yellow":
-    print("Slow down")
-elif signal == "blue" or signal == "green":
-    print("Go")
-else:
-    print("Invalid signal color...")
+# if signal == "red":
+#     print("Stop")
+# elif signal == "yellow":
+#     print("Slow down")
+# elif signal == "blue" or signal == "green":
+#     print("Go")
+# else:
+#     print("Invalid signal color...")
+
+# match
+# Python 3.10~
+
+match signal:
+    case "red":
+        print("Stop")
+    case "yellow":
+        print("Slow down")
+    case "blue" | "grenn":
+        print("Go")
+    case _:
+        print("Invalid signal color...")
