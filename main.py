3f99e950a5a08e8e03d0c3792bdbd74067ba6b1f
fu-kunn <sky53908@gmail.com>
2022-05-16 09:06:13 +0000

    [29-try..except]

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-# 真偽値
-# True それ以外
-# False 0 0.0 '' None
-
-while True:
-    command = int(input("Select 1, 2, 3 (0: Exit) "))
-    match command:
-        case 1:
-            print("Menu 1")
-        case 2:
-            print("Menu 2")
-        case 3:
-            print("Menu 3")
-        case 0:
-            break
-        case _:
-            print("Invalid command, try again")
-            continue
-    print("Menu processed correctly")
+try:
+    initial_balance = int(input("Initial Balance?" ))
+except ValueError:
+    print("Initial input, exiting...")
+    exit()
+RATE = 1.1
+for year in range(3):
+    print(f"Year {year}: {initial_balance * RATE ** year:,.2f}")
