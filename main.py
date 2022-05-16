e526fbf737842593eea8f81da873e64127dc6b83
fu-kunn <sky53908@gmail.com>
2022-05-16 08:30:42 +0000

    [22-forの中にfor]

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 initial_balance = int(input("Initial Balance? "))
 RATE = 1.1
-# yearは変数
-for year in range(10):
-    print(f"Year {year}: {initial_balance * RATE ** year:,.2f}")
-    
+
+# 1と2の時に+10000の処理を行う
+for i in range(3):
+    if i != 0:
+        initial_balance += 10_000
+    for year in range(3):
+        print(f"Year {year}: {initial_balance * RATE ** year:,.2f}")
