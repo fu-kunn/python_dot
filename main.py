a59d06b917d3df08695da50a8a250c3529f1869c
fu-kunn <sky53908@gmail.com>
2022-05-19 05:51:52 +0000

    [7-引数のデフォルト値]

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def greet(name, by):
-    print(f"{by} said, hi! {name}")
+def show_time(h, m, s=0, ms=0):
+    print(f"{h:02}:{m:02}:{s:02}:{ms:03}")
 
-greet(by="John", name="Taro") #キーワード引数
-greet("Jiro", "Rich") #位置引数
+show_time(11, 23, 52, 220)
+show_time(4, 54, 2, 12)
+show_time(12, 3)
+show_time(5, 25, 32)
