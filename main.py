4a1dbe5d92431140988e0630ed7159695f1aea56
fu-kunn <sky53908@gmail.com>
2022-05-20 02:51:09 +0000

    [17-無名関数]

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def double(n):
-    return n * 2
+# def double(n):
+#     return n * 2
 
-def triple(n):
-    return n * 3
+# def triple(n):
+#     return n * 3
 
 def calc(n, func):
     return func(n)
 
-print(calc(10, double))
-print(calc(10, triple))
+print(calc(10, lambda n: n * 2))
+print(calc(10, lambda n: n * 3))
