ca9405ea8ee430b2f535ef2305aa149d62628bf7
fu-kunn <sky53908@gmail.com>
2022-05-20 02:28:32 +0000

    [13-グローバル変数]

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def get_price(a, b):
-    if a + b >= 300:
+    global rate
+    if a + b >= 3000:
         rate = 1.05
     total = (a + b) * rate #値が代入されていない変数→外のスコープを探す
     return total
 
-# rate = 1.1
+rate = 1.1
 print(get_price(300, 700))
+print(get_price(3000, 7000))
