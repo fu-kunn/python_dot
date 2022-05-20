0c799bd089c608b96928b524a87b2cd7de1ed045
fu-kunn <sky53908@gmail.com>
2022-05-20 02:34:11 +0000

    [14-値の引数渡し]

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def get_price(a, b):
-    global rate
+def get_price(a, b, rate):
+    # global rate
     if a + b >= 3000:
         rate = 1.05
     total = (a + b) * rate #値が代入されていない変数→外のスコープを探す
     return total
 
 rate = 1.1
-print(get_price(300, 700))
-print(get_price(3000, 7000))
+print(get_price(300, 700, rate))
+print(get_price(3000, 7000, rate))
+print(rate)
