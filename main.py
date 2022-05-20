2080b4bf01aea83a99d5524508fb523ba216da36
fu-kunn <sky53908@gmail.com>
2022-05-20 02:25:03 +0000

    [12-ローカル変数になる条件]

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def get_price(a, b):
-    total = (a + b) * rate #値が代入されていない変数→外のスコープ
+    if a + b >= 300:
+        rate = 1.05
+    total = (a + b) * rate #値が代入されていない変数→外のスコープを探す
     return total
-    
-rate = 1.1
+
+# rate = 1.1
 print(get_price(300, 700))
