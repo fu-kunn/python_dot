729f1bf3e5af93a9f93a190086481d03d4f35607
fu-kunn <sky53908@gmail.com>
2022-05-25 02:14:18 +0000

    [9-スライス記法]

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # nums[2:5] = [200, 300, 400]
 
-nums[3:] = []
+# sliced_list = nums[2:5]
+# sliced_list = nums[2:8:2]
+# sliced_list = nums[8:2:-2]
+sliced_list = nums[::-1]
 
-print(nums)
+# print(nums)
+print(sliced_list)
