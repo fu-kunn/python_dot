1e3ad934ae0a373b73c5ca85d248cc32e64d0da8
fu-kunn <sky53908@gmail.com>
2022-05-25 02:34:13 +0000

    [12-リスト内包表記とifの組み合わせ]

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 prices = [100, 200, 150, 200, 100]
 
-# append リストの要素を追加する
-prices_with_tax = []
-for price in prices:
-    prices_with_tax.append(price * 1.1)
+# prices_with_tax = []
+# for price in prices:
+#     if price != 200:
+#         prices_with_tax.append(price * 1.1)
 
-prices_with_tax = [price * 1.1 for price in prices]    
+prices_with_tax = [price * 1.1 for price in prices if price != 200]
 
 print(prices_with_tax)
