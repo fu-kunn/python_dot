416f6e33228634111dd5f60b2167325b77e9bd95
fu-kunn <sky53908@gmail.com>
2022-05-26 10:46:16 +0000

    [23-2つのリストから辞書型のデータを作る方法]

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-nums = [10, 20, 30]
-# nums_bak = nums
-nums_bak = nums.copy()
-nums[0] = 100
+keys = ["math", "english", "physics"]
+values = [62, 91, 84]
 
-# どちらも100,20,30が表示される コピーメソッドを使わない
-print(nums)
-print(nums_bak)
+# scores = {}
+# for item in zip(keys, values):
+#     # print(item)
+#     key, value = item
+#     scores[key] = value
+
+scores = {key: value for key, value in zip(keys, values)}
+
+print(scores)
