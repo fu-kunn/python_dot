dfd9557eaada4b1448c29772670157975f8a87b7
fu-kunn <sky53908@gmail.com>
2022-05-26 10:26:36 +0000

    [19-集合]

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-scores = {"math":62, "english":91, "physics":88}
+#　集合
 
-# for key in scores.keys():
-#     print(key)
+members = {"Taro", "Jiro", "Saburo"}
 
-# for value in scores.values():
-#     print(value)
+# members.add("Siro")
+# members.remove("Taro")
 
-# for item in scores.items():
-#     key, value = item
-#     print(f"{key:8} {value:3}")
+# print(members)
 
-for key, value in scores.items():
-    print(f"{key:8} {value:3}")
+print(len(members))
+
+# frozen_members = frozenset(members)
+# frozen_members.add("Goro")
