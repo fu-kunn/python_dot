ea543a6f55cdcec93e9750b038731df660490e3b
fu-kunn <sky53908@gmail.com>
2022-06-05 07:42:07 +0000

    [7-日時を表す値から情報を取り出す方法]

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import datetime
 
-# now = datetime.datetime.now()
-# print(now)
 
-# birthday = datetime.datetime(year=2000, month=4, day=11)
-# 文字列から値を作る方法
-birthday = datetime.datetime.strptime("2000-04-11", "%Y-%m-%d")
-print(birthday)
+birthday = datetime.datetime(year=2000, month=4, day=11)
+# print(birthday.year)
+# print(birthday.month)
+# print(birthday.day)
+# print(birthday.minute)
+# print(birthday.weekday()) #Mon;0, Tue:1....
+birthday_formatted = birthday.strftime("%B %d %A, %Y")
+print(birthday_formatted)
