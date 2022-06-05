3130b822225827b0d6ed4d6a4ef4fe4c109fb444
fu-kunn <sky53908@gmail.com>
2022-06-05 07:00:08 +0000

    [2-randomモジュール]

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-scores = [
-  {"name": "Taro", "math":70, "english":82},
-  {"name": "Jiro", "math":67, "english":61},
-  {"name": "Saburo", "math":81, "english":58},
-  ]
+# import random
+# import random as r
+from random import randint
 
-print("Name     Math      English")
-print("-------- --------- --------")
+# n = random.random() #０以上１未満
+# n = random.randint(1, 6)
+# n = r.randint(1, 6)
+n = randint(1, 6)
 
-# def compare(score):
-#     return score["math"]
 
-# scores.sort(key=compare, reverse=True)
-scores.sort(key=lambda score: score["math"], reverse=True)
-
-for score in scores:
-    for value in score.values():
-        print(f"{value:8} ", end="")
-    # print()のインデントに注意
-    print()
+print(n)
