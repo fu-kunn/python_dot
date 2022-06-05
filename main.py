c8fcc7f1e0c07d31154b04bb92daf71126d619bc
fu-kunn <sky53908@gmail.com>
2022-06-05 08:13:39 +0000

    [10-ファイルに文字列を書き込む方法]

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import calendar
+file_name = "names.txt"
 
-#曜日を日曜日始まりにする
-# calendar.setfirstweekday(6)
-calendar.setfirstweekday(calendar.SUNDAY)
-
-# cal_str = calendar.month(2001, 1)
-# print(cal_str)
-
-#データ型取得す場合
-#週ごとの表示になっておりデータがない場合は「0」が表示される
-# cal_date = calendar.monthcalendar(2001, 1)
-# print(cal_date)
-
-#閏年がある年をtrueかfalseで返す
-print(calendar.isleap(2000))
-print(calendar.isleap(2001))
+f = open(file_name, mode="w")
+f.write("Taro\n")
+f.close()
