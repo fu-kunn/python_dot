0a1666ab89f9f19d4edf841c67e123d5e0750957
fu-kunn <sky53908@gmail.com>
2022-06-07 11:53:13 +0000

    [11-ファイルの存在をチェックして条件分岐する方法]

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import os
+
 file_name = "names.txt"
 
-f = open(file_name, mode="w")
-f.write("Taro\n")
-f.close()
+#ファイルが存在しない場合
+# if not os.path.isfile(file_name):
+#     with open(file_name, mode="w") as f:
+#         f.write("Saburo\n")
+# else:
+#     print("File exists!")
+
+#モードをxにすると新規作成専用になる→ファイルが既に存在する場合は例外を発生させる
+try:
+    with open(file_name, mode="x") as f:
+            f.write("Saburo\n")
+except FileExistsError:
+    print("File exists!")
