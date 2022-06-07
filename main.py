c55486adf61b019ecd119bb668238a7759a6a6b9
fu-kunn <sky53908@gmail.com>
2022-06-07 12:07:39 +0000

    [13-テキストファイルの内容を読み込むための処理]

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 file_name = "names.txt"
-names = ["Jiro", "Saburo", "Shiro"]
+# names = ["Taro", "Jiro", "Saburo", "Shiro"]
 
-#末尾に追記する場合は「a=(append)」
-with open(file_name, mode="a") as f:
-    for name in names:
-        f.write(f"{name}\n")
+# with open(file_name, mode="w") as f:
+#     for name in names:
+#         f.write(f"{name}\n")
+
+#r=read テキストファイルの内容を読み込む
+with open(file_name, mode="r") as f:
+    # names = f.read()
+    
+    #splitlines()=文字列を改行で分解してリスト化する
+      names = f.read().splitlines()
+print(names, end="")   
