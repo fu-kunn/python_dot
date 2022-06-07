f4649e57e544f1d4d1d2b09cb4c624c60298f421
fu-kunn <sky53908@gmail.com>
2022-06-07 11:59:55 +0000

    [12-文字列を追記する方法]

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-import os
-
 file_name = "names.txt"
+names = ["Jiro", "Saburo", "Shiro"]
 
-#ファイルが存在しない場合
-# if not os.path.isfile(file_name):
-#     with open(file_name, mode="w") as f:
-#         f.write("Saburo\n")
-# else:
-#     print("File exists!")
-
-#モードをxにすると新規作成専用になる→ファイルが既に存在する場合は例外を発生させる
-try:
-    with open(file_name, mode="x") as f:
-            f.write("Saburo\n")
-except FileExistsError:
-    print("File exists!")
+#末尾に追記する場合は「a=(append)」
+with open(file_name, mode="a") as f:
+    for name in names:
+        f.write(f"{name}\n")
